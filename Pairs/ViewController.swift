7bd3b0af588dea7261e33d6c0e64677ddfa76a21
Paul Richardson <developer@photographerafoot.com>
2021-06-24 09:36:33 +1000

    CHG: add greek alphabet

--- Pairs/ViewController.swift
+++ Pairs/ViewController.swift
@@ -11,6 +11,37 @@ typealias Alphabet = [String: (upper: Character, lower: Character)]
 
 class ViewController: UIViewController {
 
+	let greek: Alphabet = [
+		"alpha": ("Α", "α"),
+		"beta": ("Β", "β"),
+		"gamma": ("Γ", "γ"),
+		"delta": ("Δ", "δ"),
+		"epsilon": ("Ε", "ε"),
+		"digamma": ("Ϝ", "ϝ"),
+		"zeta": ("Ζ", "ζ"),
+		"eta": ("Η", "η"),
+		"theta": ("Θ", "θ"),
+		"iota": ("Ι", "ι"),
+		"kappa": ("Κ", "κ"),
+		"lambda": ("Λ", "λ"),
+		"mu": ("Μ", "μ"),
+		"nu": ("Ν", "ν"),
+		"xi": ("Ξ", "ξ"),
+		"omicron": ("Ο", "ο"),
+		"pi": ("Π", "π"),
+		"san": ("Ϻ", "ϻ"),
+		"qoppa": ("Ϙ", "ϙ"),
+		"rho": ("Ρ", "ρ"),
+		"sigma": ("Σ", "σ"),
+		"tau": ("Τ", "τ"),
+		"upsilon": ("Υ", "υ"),
+		"phi": ("Φ", "φ"),
+		"chi": ("Χ", "χ"),
+		"psi": ("Ψ", "ψ"),
+		"omega": ("Ω", "ω"),
+		"sampi": ("Ϡ", "ϡ"),
+	]
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
